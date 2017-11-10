d252e2b80fa4fd0effc3b604a836cc5f814637c5
Hanna Kalinowska <hanna.kalinowska@simplybusiness.co.uk>
2017-11-10 15:04:37 +0000

    CHAOS MONKEY
    BANANANANANANA

---
+++ lib/chaos_monkey.rb
@@ -0,0 +1,20 @@
+class ChaosMonkey
+  def initialize(next_handler)
+    @next_handler = next_handler
+  end
+
+  def handle(message)
+    chaos = rand(100)
+
+    case chaos
+    when 0 .. 10
+      puts Rainbow("MESSAGE DROPPED MWAHAHAHA").bright.red
+    when 85 .. 100
+      puts Rainbow("BANANANANANA").bright.red
+      @next_handler.handle(message)
+      @next_handler.handle(message)
+    else
+      @next_handler.handle(message)
+    end
+  end
+end
